e237d00d74bfbd66139c2b203c32fe229195dea0
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-01-28 20:37:09 +0700

    membuat service untuk lokasi

---
+++ app/src/main/java/com/example/a6129/service/LokasiService.kt
@@ -0,0 +1,32 @@
+package com.example.a6129.service
+
+import com.example.a6129.models.Lokasi
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface LokasiService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+    @GET("bacalokasi.php")
+    suspend fun getLokasi(): List<Lokasi>
+
+    @GET("baca1lokasi.php/{id_lokasi}")
+    suspend fun getLokasiById(@Query("id_lokasi") id_lokasi: String): Lokasi
+
+    @POST("insertlokasi.php")
+    suspend fun insertLokasi(@Body lokasi: Lokasi)
+
+    @PUT("editlokasi.php")
+    suspend fun updateLokasi(@Query("id_lokasi")id_lokasi: String, @Body lokasi: Lokasi)
+
+    @DELETE("deletelokasi.php/{id_lokasi}")
+    suspend fun deleteLokasi(@Query("id_lokasi")id_lokasi: String): Response<Void>
+}
