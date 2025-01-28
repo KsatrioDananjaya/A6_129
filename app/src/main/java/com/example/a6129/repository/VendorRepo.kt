cf8ef7179801370974dfd2738584cf2a4b7f6255
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-01-28 20:41:30 +0700

    menambahkan network di repo vendor untuk crud

--- app/src/main/java/com/example/a6129/repository/VendorRepo.kt
+++ app/src/main/java/com/example/a6129/repository/VendorRepo.kt
@@ -1,6 +1,7 @@
 package com.example.a6129.repository
 
 import com.example.a6129.models.Vendor
+import com.example.a6129.service.VendorService
 
 interface VendorRepo {
     suspend fun getVendor(): List<Vendor>
@@ -8,4 +9,37 @@ interface VendorRepo {
     suspend fun updateVendor(id_vendor: String, vendor: Vendor)
     suspend fun deleteVendor(id_vendor: String)
     suspend fun getVendorById(id_vendor: String): Vendor
+}
+
+class NetworkVendorRepository(private val vendorService: VendorService)
+    : VendorRepo
+{
+    override suspend fun getVendor(): List<Vendor> = vendorService.getVendor()
+
+    override suspend fun insertVendor(vendor: Vendor) {
+        vendorService.insertVendor(vendor)
+    }
+
+    override suspend fun updateVendor(id_vendor: String, vendor: Vendor) {
+        vendorService.updateVendor(id_vendor, vendor)
+    }
+
+    override suspend fun deleteVendor(id_vendor: String) {
+        try {
+            val response = vendorService.deleteVendor(id_vendor)
+            if (!response.isSuccessful) {
+                throw Exception("Failed to delete lokasi. HTTP Status Code: ${response.code()}")
+            }
+            else{
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getVendorById(id_vendor: String): Vendor {
+        return vendorService.getVendorById(id_vendor)
+    }
 }
