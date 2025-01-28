31d0645c13b4de6a1a9b2458e88daa8684f621e4
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-01-28 20:39:37 +0700

    menambahkan network di repo klien untuk crud

--- app/src/main/java/com/example/a6129/repository/ClientRepo.kt
+++ app/src/main/java/com/example/a6129/repository/ClientRepo.kt
@@ -9,3 +9,39 @@ interface ClientRepo {
     suspend fun deleteKlien(id_klien: String)
     suspend fun getKlienById(id_klien: String): Client
 }
+
+class NetworkClientRepository(
+    private val clientService: ClientService) : ClientRepo {
+
+    override suspend fun insertKlien(klien: Client) {
+        clientService.insertKlien(klien)
+    }
+
+    override suspend fun updateKlien(id_klien: String, klien: Client) {
+        clientService.updateKlien(id_klien, klien)
+    }
+
+    override suspend fun deleteKlien(id_klien: String) {
+        try {
+            val response = clientService.deleteKlien(id_klien)
+            if (!response.isSuccessful) {
+                throw Exception("Failed to delete klien. HTTP Status code: ${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getKlien(): List<Client> = clientService.getKlien()
+
+    override suspend fun getKlienById(id_klien: String): Client {
+        try {
+            return clientService.getKlienById(id_klien)
+        } catch (e: Exception) {
+            throw Exception("Failed to fetch client with ID: $id_klien. Network error occurred.", e)
+        }
+    }
+}
