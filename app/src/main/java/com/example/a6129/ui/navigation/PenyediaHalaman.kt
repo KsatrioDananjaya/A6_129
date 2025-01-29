ff0509a6be54a9969d294a2da26c4c87abe898ed
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-01-29 11:12:41 +0700

    Mmebuat pengelolahalaman

--- app/src/main/java/com/example/a6129/ui/navigation/PenyediaHalaman.kt
+++ app/src/main/java/com/example/a6129/ui/navigation/PenyediaHalaman.kt
@@ -1,2 +1,40 @@
 package com.example.a6129.ui.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.a6129.ui.view.HomeApp
+
+@Composable
+fun PengelolaHalaman(
+    modifier : Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiMain.route,
+        modifier = Modifier,
+    ) {
+        composable(route = DestinasiMain.route) {
+            HomeApp(
+                onAcara = {
+                    navController.navigate(DestinasiAcara.route)
+                },
+                onKlien = {
+                    navController.navigate(DestinasiKlien.route)
+                },
+                onLokasi = {
+                    navController.navigate(DestinasiLokasi.route)
+                },
+                onVendor = {
+                    navController.navigate(DestinasiVendor.route)
+                }
+            )
+        }
+    }
+}
