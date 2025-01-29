3a3f41786b978afce4ae65446b4cc897adf3bd88
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-01-29 08:42:12 +0700

    menambahkan acaraapp

--- app/src/main/java/com/example/a6129/EventApp.kt
+++ app/src/main/java/com/example/a6129/EventApp.kt
@@ -1,32 +1,14 @@
 package com.example.a6129
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
+import android.app.Application
+import com.example.a6129.dependenciesinjection.AppContainer
+import com.example.a6129.dependenciesinjection.ContainerApp
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun EventApp(
-    modifier: Modifier = Modifier
-) {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+class AcaraApplication : Application() {
+    lateinit var container: AppContainer
 
-    Scaffold(
-        modifier = Modifier
-            .nestedScroll(scrollBehavior.nestedScrollConnection),
-    ) {
-        Surface(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(it)
-        ) {
-            PengelolaHalaman()
-        }
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerApp()
     }
 }
